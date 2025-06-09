bbbcb3f248dd6fc430429e9efb0fc2cfd6f066ce
kottybarn3567 <kaualopes3567@gmail.com>
2025-06-09 19:06:24 -0300

    Update select_from_nobel.sql

--- select_from_nobel.sql
+++ select_from_nobel.sql
@@ -1,25 +1,25 @@
-// 01
+--- 01
 SELECT yr, subject, winner FROM nobel
 WHERE yr = 1950;
 
-// 02
+--- 02
 SELECT winner FROM nobel
 WHERE yr = 1962 AND subject = 'literature';
 
-// 03
+--- 03
 SELECT yr AS year, subject FROM nobel
 WHERE winner = 'Albert Einstein';
 
-// 04
+--- 04
 SELECT winner as winners FROM nobel
 WHERE yr >= 2000 AND subject = 'peace';
 
-// 05
+--- 05
 SELECT * FROM nobel
 WHERE subject = 'literature'
 AND yr BETWEEN 1980 AND 1989;
 
-// 06
+--- 06
 SELECT * FROM nobel
  WHERE winner IN
 (
@@ -29,11 +29,11 @@ SELECT * FROM nobel
    'Barack Obama'
 );
 
-// 07
+--- 07
 SELECT winner FROM nobel
 WHERE winner LIKE 'John%';
 
-// 08
+--- 08
 SELECT * FROM nobel
 WHERE (subject, yr)
  IN (
@@ -41,41 +41,41 @@ WHERE (subject, yr)
    ('chemistry', 1984)
  );
 
-// 08.2
+--- 08.2
 SELECT * FROM nobel
 WHERE subject = 'physics' AND yr = 1980
 OR subject = 'chemistry' AND yr = 1984;
 
-// 09
+--- 09
 SELECT * FROM nobel
 WHERE yr = 1980 AND
 subject  <> 'chemistry' AND
 subject <> 'medicine';
 
-// 09.2
+--- 09.2
 SELECT * FROM nobel
 WHERE yr = 1980 AND
 subject NOT IN('chemistry', 'medicine');
 
-// 10
+--- 10
 SELECT * FROM nobel
 WHERE subject = "medicine" AND yr < 1910
 OR subject = "literature" AND yr >= 2004;
 
-// 11
+--- 11
 SELECT * FROM nobel
 WHERE winner = 'Peter Grünberg';
 
-// 12
+--- 12
 SELECT * FROM nobel
 WHERE winner = 'Eugene O''Neill';
 
-// 13
+--- 13
 SELECT winner, yr, subject FROM nobel
 WHERE winner LIKE 'Sir%'
 ORDER BY yr DESC, winner ASC;
 
-// 14
+--- 14
 SELECT winner, subject FROM nobel
 WHERE yr = 1984
 ORDER BY FIELD(subject, 'chemistry', 'physics'), subject, winner
